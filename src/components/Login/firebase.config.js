5891982270151e70d57cb72f82899597855d01fb
hannan-abdul <shohas563@gmail.com>
2021-03-18 18:34:45 +0600

    all

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCmb7GHqRg1zwiI-hXLaz-cIxwUkISFlEU",
+    authDomain: "burj-al-arab-uae.firebaseapp.com",
+    projectId: "burj-al-arab-uae",
+    storageBucket: "burj-al-arab-uae.appspot.com",
+    messagingSenderId: "182801236148",
+    appId: "1:182801236148:web:d2659ee6f89586067557e1"
+  };
+  export default firebaseConfig;
