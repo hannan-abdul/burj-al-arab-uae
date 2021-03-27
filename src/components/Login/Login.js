2e914304bdac99ca00c601288d729996a02cbaa8
hannan-abdul <shohas563@gmail.com>
2021-03-27 12:10:50 +0600

    some login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
     const location = useLocation();
     const { from } = location.state || { from: { pathname: "/" } };
 
-    if(firebase.apps.length === 0){
+    if (firebase.apps.length === 0) {
         firebase.initializeApp(firebaseConfig);
     }
     const handleGoogleSignIn = () => {
@@ -20,9 +20,10 @@ const Login = () => {
         firebase.auth()
             .signInWithPopup(provider)
             .then((result) => {
-                const {displayName, email} = result.user;
-                const signedInUser = {name: displayName, email};
+                const { displayName, email } = result.user;
+                const signedInUser = { name: displayName, email };
                 setLoggedInUser(signedInUser);
+                storeAuthToken();
                 history.replace(from);
             }).catch((error) => {
                 var errorCode = error.code;
@@ -31,10 +32,40 @@ const Login = () => {
                 var credential = error.credential;
             });
     }
+    const handleFacebookSignIn = () => {
+        var fbProvider = new firebase.auth.FacebookAuthProvider();
+        firebase
+            .auth()
+            .signInWithPopup(fbProvider)
+            .then((result) => {
+                const { displayName, email } = result.user;
+                const signedInUser = {name: displayName, email};
+                setLoggedInUser(signedInUser);
+                history.replace(from);
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                var email = error.email;
+                var credential = error.credential;
+            });
+    }
+
+    const storeAuthToken = () => {
+        firebase.auth().currentUser.getIdToken(/* forceRefresh */ true)
+        .then(function(idToken) {
+            sessionStorage.setItem('token', idToken);
+          }).catch(function(error) {
+            // Handle error
+          });
+    }
+
     return (
-        <div>
+        <div style={{textAlign: 'center'}}>
             <h1>This is Login</h1>
             <button onClick={handleGoogleSignIn}>Google Sign in</button>
+            <br/>
+            <button onClick={handleFacebookSignIn}>Facebook Sign in</button>
         </div>
     );
 };
